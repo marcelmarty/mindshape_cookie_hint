87cf814ee0010736f54ca361d4574b44d1cb8cb3
Marcel Marty <contact@marcelmarty.ch>
2025-09-05 17:07:40 +0200

    [CHANGE] Removed not necessary files

--- Overrides/sys_template.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('mindshape_cookie_hint', 'Configuration/TypoScript', 'mindshape Cookie Hint');
--- tt_content.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin('mindshape_cookie_hint', 'Main', 'Cookies');
